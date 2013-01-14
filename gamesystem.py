6c586af28537cba7123311d88964314a71c877e3
ste920ven <ste920ven@gmail.com>
2013-01-14 14:38:11 -0800

    started gamesystem

---
+++ gamesystem.py
@@ -0,0 +1,23 @@
+import util
+
+class game:
+  def __init_(self):
+
+  def end(self):
+  
+  def startGame(self):
+
+  def eliminate(self,player):
+    
+
+class player:
+target = NULL
+
+  def __init__(self):
+
+  def updateLoc(self,loc):
+  
+  def updateMap(self):
+
+  def getKill(self,player):
+    target = player.target
