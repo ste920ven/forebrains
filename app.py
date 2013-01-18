85bd8d1060dea0ab6819c1e7b59ff482c18c4af3
Nathaniel Biggs <biggs0125@gmail.com>
2013-01-18 00:37:15 -0500

    signup/login working

--- app.py
+++ app.py
@@ -1,15 +1,66 @@
-from flask import request,Flask,render_template, url_for,redirect,request
-import urllib2,json
+from flask import request,Flask,render_template, url_for,redirect,request,session
+import urllib2,json,util
 
 app=Flask(__name__)
+app.secret_key = "JackStevenDinaandBiggsAreAwesomeExceptNotReallyDina1"
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def index():
-    return render_template("index.html")
+    if request.method=="GET":
+        return render_template("index.html")
+    else:
+        pending = request.form.keys()[0]
+        if "tab" in pending:
+            return handleTabs(pending)
 
-@app.route("/login")
+@app.route("/login",methods=["POST","GET"])
 def login():
-    return render_template("login.html")	
+    if request.method=="GET":
+        return render_template("login.html")	
+    else:
+        pending = request.form.keys()[0]
+        if "tab" in pending:
+            return handleTabs(pending)
+        else: 
+            if request.form.has_key("submitlogin"):
+                user = str(request.form["Username"])
+                password = str(request.form["Password"])
+                validate = util.checkUserPass(user,password)
+                if validate == 0:
+                    #User doesn't exist
+                    return render_template("login.html")
+                if validate == True:
+                    session['user'] = user
+                    return redirect(url_for("index"))
+                if validate == False:
+                    #Password Incorrect
+                    return render_template("login.html")
+
+@app.route("/signup",methods=["POST","GET"])
+def signup():
+    if request.method=="GET":
+        return render_template("signup.html")
+    else:
+        pending = request.form.keys()[0]
+        if "tab" in pending:
+            return handleTabs(pending)
+        user = str(request.form["user"])
+        password = str(request.form["pass1"])
+        if password != str(request.form["pass2"]):
+            return render_template("signup.html",notmatching=True)
+        if util.createUser(user,password):
+            return redirect(url_for("login"))
+        else:
+            return render_template("signup.html",taken=True)
+
+def handleTabs(pressed):
+    if "home" in pressed:
+        return redirect(url_for("index"))
+    if "login" in pressed:
+        return redirect(url_for("login"))
+    if "signup" in pressed:
+        return redirect(url_for("signup"))
+
 
 if __name__=="__main__":
     app.debug=True
--- util.py
+++ util.py
@@ -13,34 +13,41 @@ users = db.FinalProjUsers
 games = db.FinalProjGames
 
 def createUser(user,password):
+    if users.find_one({"user" : user}) != None:
+        return False
     tmp = base64.b64encode(password)
     newuser = {"user" : user, "pass" : tmp, "game" : 0, "wins" : 0, "loses" : 0,"friends" : []}
     users.insert(newuser)
+    return True
 
 def createGame(creator,password,name):
     if password != "":
         tmp = base64.b64encode(password)
     else:
         tmp = False
-    newgame = {"creator" : creator, "pass" : tmp, "name" : name, "players" : {creator : {"loc":[0,0], "pursuer" : "", "target" : "", "kills" = 0, "live" : False}}}
+    newgame = {"creator" : creator, "pass" : tmp, "name" : name, creator : {"loc":[0,0], "pursuer" : "", "target" : "", "kills" : 0, "live" : False}}
     games.insert(newgame)
 
 def addPlayer(user,game):
     tmp = games.find_one({"name":game})["players"]
-    tmp[user] = {"loc":[0,0], "pursuer" : "", "target" : "", "kills" = 0, "live" : False}
+    tmp[user] = {"loc":[0,0], "pursuer" : "", "target" : "", "kills" : 0, "live" : False}
     games.update({"name" : game},{"$set" : {"players" : tmp}})
 
-def checkuserpass(user,password):
+def checkUserPass(user,password):
     encpass = base64.b64encode(password)
     tmp = users.find_one({"user" : user})
+    if tmp == None:
+        return 0
     if encpass == tmp["pass"]:
         return True
     else:
         return False
 
-def checkgamepass(game,password):
+def checkGamePass(game,password):
     encpass = base64.b64encode(password)
     tmp = games.find_one({"name":game})
+    if tmp == None:
+        return 0
     if encpass == tmp["pass"]:
         return True
     else:
@@ -48,15 +55,16 @@ def checkgamepass(game,password):
 
 def startGame(game):
     tmp = games.find_one({"name":game})
-    k = tmp.keys()
-    targets = tmp.keys()
-    random.shuffle(targets)
+    players = tmp.keys()
+    random.shuffle(players)
     current = 0
-    for person in k:
-        tmp[person]["live"] = True
-        tmp[person]["target"] = targets[current]
+    for person in players:
+        if current == len(players):
+            current = -1
+        tmp[person]["target"] = players[current+1]
+        tmp[players[current+1]]["pursuer"] = person
         current = current + 1
-    games.update({"name":game},{"$set":game})
+    games.update({"name":game},{"$set":tmp})
     return True
        
 def getTarget(player,game):
@@ -94,7 +102,13 @@ def getAllLocs(game):
     locations = []
     tmp = games.find_one({"name":game})
     k = tmp.keys()
+    invalids = ["creator","pass","name"]
     for person in k:
+        if person not in invalids:
+            locations.append(tmp[person]["loc"])
+    return locations
+
+                     
         
 
 #encode password for check
