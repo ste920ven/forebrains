dad9e6ff40251bd37c970357d2bc49d982265a92
kajabla <jskestrel@gmail.com>
2013-01-18 12:55:13 -0500

    signup is go

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__=="__main__":
     app.debug=True
     app.run()
